2a296ce91f502a61f409390fb775f18bdef372be
y <codecharliecode@gmail.com>
2021-06-22 20:44:37 +0100

    Added id's to both the user and workout class. Initialised a new class 'Exercise' within the exercise.py file inside the models directory

---
+++ models/exercise.py
@@ -0,0 +1,10 @@
+class Exercise:
+
+    def __init__(self, name_of_exericise, rep_max, id=None):
+        self.name_of_exercise = name_of_exericise
+        self.rep_max = rep_max
+        self.id = id
+
+#Rep Max - also known as "1rm" or "1 rep max": https://en.wikipedia.org/wiki/One-repetition_maximum
+
+
