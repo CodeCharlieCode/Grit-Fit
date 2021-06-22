b3f426cd3175c49b82f7ae0189a91c50e09b5e4d
y <codecharliecode@gmail.com>
2021-06-22 20:34:05 +0100

    Setting up files and directorys, created a user class within the user.py file

---
+++ models/user.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self, first_name, last_name, age, weight, height):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.weight = weight
+        self.height = height
