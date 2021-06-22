bb662d49662d83f1defc312416b1bfc5a7e28786
y <codecharliecode@gmail.com>
2021-06-22 20:37:11 +0100

    Created a Workout class within the workout.py file inside the models directory

---
+++ models/workout.py
@@ -0,0 +1,8 @@
+class Workout:
+
+    def __init__(self, exercise, repetitions, sets, rest_time, comments):
+        self.exercise = exercise
+        self.repetitions = repetitions
+        self.sets = sets
+        self.rest_time = rest_time
+        self.comments = comments
