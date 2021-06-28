1b957640fe0cbc0173cc13bacee18c66860d25da
y <codecharliecode@gmail.com>
2021-06-28 19:20:10 +0100

    Added two more tests, testing: height and id

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -6,7 +6,7 @@ class TestUser(  unittest.TestCase  ):
 
     def setUp(self):
         self.user_1 = User("Matt", "Round", "male", 32, 90.82, 183, 8)
-        self.user_2 = User("Hannah", "Black", "female", 26, 57.6, 167)
+        self.user_2 = User("Hannah", "Black", "female", 26, 57.6, 167, 10)
 
     def test_first_name(self):
         expected = "Matt"
@@ -31,5 +31,15 @@ class TestUser(  unittest.TestCase  ):
     def test_weight(self):
         expected = 90.82
         actual = self.user_1.weight
+        self.assertEqual(expected, actual)
+
+    def test_height(self):
+        expected = 167
+        actual = self.user_2.height
+        self.assertEqual(expected, actual)
+
+    def test_id(self):
+        expected = 8
+        actual = self.user_1.id
         self.assertEqual(expected, actual)
 
