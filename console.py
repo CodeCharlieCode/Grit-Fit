c7ae7c68e7e739854ba54b7cbc203a66328776a3
y <codecharliecode@gmail.com>
2021-08-06 20:23:06 +0100

    Populated the console.py file with an object and corrected a spelling mistake in templates/exercises/index.html

--- console.py
+++ console.py
@@ -1,3 +1,7 @@
 from models.exercise import Exercise
 from models.user import User
 from models.workout import Workout
+import repositories.exercise_repository as exercise_repository 
+
+exercise1 = Exercise("Strength","Squat", 100, 98, 1)
+exercise_repository.save(exercise1)
--- repositories/exercise_repository.py
+++ repositories/exercise_repository.py
@@ -2,7 +2,7 @@ from models.exercise import Exercise
 from db.run_sql import run_sql
 
 def save(exercise):
-    sql = "INSERT INTO merchants (exercise_type, name_of_exercise, rep_max, personal_best) VALUES (%s, %s, %s, %s) RETURNING id"
+    sql = "INSERT INTO exercises (exercise_type, name_of_exercise, rep_max, personal_best) VALUES (%s, %s, %s, %s) RETURNING id"
     values = [exercise.exercise_type, exercise.name_of_exercise, exercise.rep_max, exercise.personal_best]
     results = run_sql(sql, values)
     exercise.id = results[0]['id']
