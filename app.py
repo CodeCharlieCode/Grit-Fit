bfd7df101a1e0d2f510d9fb96a74255df7ede277
y <codecharliecode@gmail.com>
2021-08-06 19:57:30 +0100

    created a new index.html file with the exercises directory within templates - tested if the page rendered text - running fine

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+from controllers.exercise_controller import exercise_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(exercise_blueprint)
 
 @app.route("/")
 def main():
