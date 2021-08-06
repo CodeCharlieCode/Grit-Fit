7f70a33f935c444a45be92d27c71efa8c5c75ad9
y <codecharliecode@gmail.com>
2021-08-06 23:37:01 +0100

    Added css styling and displayed the exercises in a table format

--- console.py
+++ console.py
@@ -5,3 +5,5 @@ import repositories.exercise_repository as exercise_repository
 
 exercise1 = Exercise("Strength","Squat", 100, 98, 1)
 exercise_repository.save(exercise1)
+exercise2 = Exercise("Strength", "Bench Press", 70, 65, 2)
+exercise_repository.save(exercise2)
